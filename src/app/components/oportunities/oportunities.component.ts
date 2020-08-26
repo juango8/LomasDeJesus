1ae069638551d4f8b590848c605e0982b62bb9aa
adrianaguu <abel.hancco@ucsp.edu.pe>
2020-08-26 08:38:32 -0500

    pdfs

--- src/app/components/oportunities/oportunities.component.ts
+++ src/app/components/oportunities/oportunities.component.ts
@@ -16,4 +16,12 @@ export class OportunitiesComponent implements OnInit {
     window.open('../../../assets/pdf/LOMAS-DE-JESÚS-HISTÓRICA-Y-VIGENTE-PROPIEDAD-PRIVADA.pdf');
   }
 
+  openPDF1(){
+    window.open('../../../assets/pdf/rd-0595-2019.pdf');
+  }
+
+  openPDF2(){
+    window.open('../../../assets/pdf/escaneo.pdf');
+  }
+
 }
